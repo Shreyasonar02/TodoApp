7afda0857d033387c3caa704c7e1ba755e273017
Shreyasonar02 <shreyasonar30@gmail.com>
2023-11-21 11:25:55 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="SearchPage">
-      <h2>Rules and Regulation</h2>
+    <div className="Header">
+      <h4>Rules and Regulation</h4>
+      <ol type="I">
+      <li>Task due date is required.</li>
+        <li>Please set an appropriate date.</li>
+   </ol>
     
     </div>
   );
