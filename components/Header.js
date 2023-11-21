6603de925acf62e05590b7273a78d6f566d3b18c
Shreyasonar02 <shreyasonar30@gmail.com>
2023-11-21 10:57:33 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,12 @@
+import React from "react";
 
-function home() {
-    const history = useHistory();
-    const handleClick = () =>history.push("/search")
-      return (
-        <div className='Home-Main'>
-          <div className='Header'>
-            <img src={Gmicon} alt=''/>
-            </div>
-            <div className='first-container'>
-              <img src={image2} alt=''/>
-            </div>
-            <div className='podcast'>
-              <img src={podcast} alt=''/>
-            </div>
-            <div className='icons'>
-              <img src={HomeBtn} alt=''/>
-              <img src={srchbtn} alt='' onClick={handleClick}/>
-              <img src={librarybtn} alt=''/>
-            </div>
-        </div>
-      
-      );
-    }
+function Header() {
+  return (
+    <div className="SearchPage">
+      <h2>Rules and Regulation</h2>
     
-    export default Home;
+    </div>
+  );
+}
+
+export default Header;
